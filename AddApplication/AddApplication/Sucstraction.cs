3aef70d35747981ee56a27ea3ac5d65ad1028ea5
rajca <rajacn.net@gmail.com>
2018-09-26 16:05:38 -0500

    Substraction functionality added

---
+++ AddApplication/AddApplication/Sucstraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddApplication
+{
+    class Sucstraction
+    {
+        public void Subtraction()
+        {
+            int a1 = 34;
+            int a2 = 45;
+
+            Console.WriteLine("Substraction {0}", a1 - a2);
+            Console.ReadLine();
+
+        }
+    }
+}
