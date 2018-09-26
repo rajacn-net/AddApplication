072752c3a0bcf23c0d8372e03cc299bb234784c5
Canna <ucmo.canna@gmail.com>
2018-09-26 14:56:04 -0500

    Add Functionality added.

---
+++ AddApplication/AddApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 12;
+            int num2 = 14;
+            Console.WriteLine("addition is {0}", num1 + num2);
+            Console.ReadLine();
+        }
+    }
+}
